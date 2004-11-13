a1d8f301073d0c78daa1097a204aa4a2dd20da7c
borisk <>
2004-11-13 05:18:47 +0000

    Added comments for 'struct schedule_helper'.

--- src/sched_util.h
+++ src/sched_util.h
@@ -11,16 +11,26 @@ struct abstract_element
 
 struct schedule_helper
 {
-  double dt,dt_1,now;
-  int buf_len,index,count;
-  int *circ_buf_count;
-  struct abstract_element **circ_buf_head;
-  struct abstract_element **circ_buf_tail;
-  struct abstract_element *current;
-  struct abstract_element *current_tail;
-  int current_count;
+  double dt;				/* timestep per slot */
+  double dt_1;				/* dt_1 = 1/dt */
+  double now;				/* start time of the scheduler */
+  int buf_len;				/* number of slots in the scheduler */
+  int index;				/* points to the next time block */
+  int count;				/* number of future items */
+  int *circ_buf_count;			/* array of counts of items that will
+					   happen at time equal to the index 
+					   value of the array */
+  struct abstract_element **circ_buf_head;   /* array of the heads of the linked
+						lists for future items */
+  struct abstract_element **circ_buf_tail;   /* array of the tails of the linked
+						lists for future items */
+  struct abstract_element *current;	   /* points to the current item */
+  struct abstract_element *current_tail;   /* points to the tail of the linked
+					      list of the current items */
+  int current_count;			   /* number of current items */
   struct schedule_helper *next_scale;
-  int error;
+  int error;				   /* error code (1 - on error, 0 -
+						no errors) */
 };
 
 struct abstract_element* ae_list_sort(struct abstract_element *ae);
