030f6390b44405dba082ebc3eaf1168b80a5c9f5
borisk <>
2006-09-26 02:09:41 +0000

    Fixed the name of the function 'jenkins_hash()' ( was 'jerkins').

--- src/sym_table.h
+++ src/sym_table.h
@@ -4,12 +4,12 @@
 #include "mcell_structs.h"
 
 #ifndef ISAAC64_H
-/* These guys come in for free if we're using Jerkins' random numbers */
+/* These guys come in for free if we're using Jenkins' random numbers */
 typedef uint32_t ub4;   /* unsigned 4-byte quantities */
 typedef unsigned char ub1;        /* unsigned 1-byte quantities */
 #endif
 
-ub4 jerkins_hash(ub1 *sym);
+ub4 jenkins_hash(ub1 *sym);
 unsigned long hash(char *sym);
 struct sym_table *retrieve_sym(char *sym, unsigned short sym_type,
   struct sym_table **hashtab);
