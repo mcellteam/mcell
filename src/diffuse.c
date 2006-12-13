b6a16bc9a55f61002d230ffa7e16cf3101e51282
kerr <>
2006-12-13 23:37:08 +0000

    Author: Rex Kerr
    There was a sign error in my derivation; the distribution of times should be
    correct now.  Also, put makedefs back to defaulting to optimization and no
    debugging.

--- src/diffuse.c
+++ src/diffuse.c
@@ -108,7 +108,7 @@ void pick_clamped_displacement(struct vector3 *v,struct volume_molecule *m)
   r_n = world->r_step_surface[ n & (world->radial_subdivisions-1) ];
   
   p = one_over_2_to_20th*((n>>12)+0.5);
-  t = r_n/erfcinv((p+1)*erfc(r_n));
+  t = r_n/erfcinv(p*erfc(r_n));
   pick_2d_displacement(&r_uv,sqrt(t)*m->properties->space_step); 
   
   r_n *= m->index * m->properties->space_step;
