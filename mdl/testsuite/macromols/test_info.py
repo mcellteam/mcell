bd4fd8ff81fb8fbc09fe56ad4d66873e98b879fa
Jed Wing <jed@salk.edu>
2008-09-16 18:15:06 -0700

    Updated testsuite to include complete surface macromol numerical validation.

--- mdl/testsuite/macromols/test_info.py
+++ mdl/testsuite/macromols/test_info.py
@@ -1,6 +1,7 @@
-from test_macromols import errorsuite, numericsuite
+from test_macromols import errorsuite, numericsuite, tmpsuite
 
 tests = {
+    "tmpsuite"          : "whatever test(s) I'm currently working on",
     "errorsuite"        : "Test error handling for invalid macromolecule constructs in MDL files",
     "numericsuite"      : "Numeric validation for Macromolecules",
 }
