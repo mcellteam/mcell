93ffc30b4cb192446f1dbdcf4269d3fb367891d9
Jed Wing <jed@salk.edu>
2009-03-28 00:07:02 -0700

    Added regression test for the recent parser bug Boris reported.

--- mdl/testsuite/regression/test_regression.py
+++ mdl/testsuite/regression/test_regression.py
@@ -127,5 +127,10 @@ class TestRegressions(unittest.TestCase):
     mt.set_check_std_handles(1, 1, 1)
     mt.invoke(get_output_dir())
 
+  def test_011(self):
+    mt = McellTest("regression", "11-quoted_tickmark_counts_parse_error.mdl", ["-quiet"])
+    mt.set_check_std_handles(1, 1, 1)
+    mt.invoke(get_output_dir())
+
 def suite():
   return unittest.makeSuite(TestRegressions, "test")
