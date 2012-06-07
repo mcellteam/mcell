da45717658003c910866da901c40e04b660c1a14
Jeffrey Bush <jeff@coderforlife.com>
2012-06-06 22:15:22 -0700

    Using strerror_s in place of strerror_r for Windows builds. Added windows-lib.h for Windows-specific defininitions. First Windows-specific commit.

---
+++ src/windows-lib.h
@@ -0,0 +1,14 @@
+#ifndef WINDOWS_LIB
+#define WINDOWS_LIB
+
+#ifdef _WIN32
+
+//#define MINGW_HAS_SECURE_API /* required for MinGW to expose _s functions */
+
+#include <string.h>
+
+// MinGW does not include this in any header but has it in the libraries
+_CRTIMP errno_t __cdecl strerror_s(char *_Buf,size_t _SizeInBytes,int errnum);
+#endif
+
+#endif
