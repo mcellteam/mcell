f9439541a05b15e2c645e017ded10972b56aa432
Markus Dittrich <haskelladdict@gmail.com>
2013-10-10 15:36:29 -0400

    Removed extern struct volume *world from util.c

--- src/util.c
+++ src/util.c
@@ -16,7 +16,6 @@
 #include "util.h"
 #include "mcell_structs.h"
 
-extern struct volume *world;
 
 /********************************************************************
 Infinite array - routines to handle infinite arrays.
