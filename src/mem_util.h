27edf1bd4e5c7778ff848b2601ec34d77fab927e
Jacob Czech <czech.jacob@gmail.com>
2014-09-12 09:11:53 -0400

    Removed old code.
    Removed code that was commented out in previous commit.

--- src/mem_util.h
+++ src/mem_util.h
@@ -77,35 +77,6 @@ void *checked_mem_get(struct mem_helper *mh, char const *file,
 #define CHECKED_SPRINTF(fmt, ...)                                              \
   checked_alloc_sprintf(__FILE__, __LINE__, CM_EXIT, fmt, ##__VA_ARGS__)
 
-/* counter_header and counter_helper not used by MCell3 */
-struct counter_header {
-  struct counter_header *next;
-  struct counter_header *prev;
-  int n;
-};
-
-struct counter_helper {
-  struct mem_helper *mem;
-  struct counter_header *head;
-  int data_size;
-  int n_unique;
-};
-
-/* stack_helper not used by MCell3 */
-struct stack_helper {
-  int index;
-  int record_size;
-  int length;
-  unsigned char *data;
-  struct stack_helper *next;
-  struct stack_helper *defunct;
-};
-
-/* temp_mem not used by MCell3 */
-struct temp_mem {
-  struct stack_helper *pointers;
-};
-
 /* Everything allocated by a mem_helper must start with a next pointer
    as if it were derived from an abstract_list */
 struct abstract_list {
@@ -140,30 +111,6 @@ void *mem_get(struct mem_helper *mh);
 void mem_put(struct mem_helper *mh, void *defunct);
 void mem_put_list(struct mem_helper *mh, void *defunct);
 void delete_mem(struct mem_helper *mh);
-
-#if 0
-struct counter_helper *create_counter(int size, int length);
-int counter_add(struct counter_helper *ch, void *data);
-void counter_reset(struct counter_helper *ch);
-struct counter_header *counter_iterator(struct counter_helper *ch);
-struct counter_header *counter_next_entry(struct counter_header *c);
-int counter_howmany(struct counter_header *c);
-void counter_read(struct counter_helper *ch, struct counter_header *c,
-                  void *data);
-void delete_counter(struct counter_helper *ch);
-
-struct stack_helper *create_stack(int size, int length);
-void *stack_push(struct stack_helper *sh, void *d);
-void stack_pop(struct stack_helper *sh, void *d);
-void stack_dump(struct stack_helper *sh);
-int stack_size(struct stack_helper *sh);
-void *stack_access(struct stack_helper *sh, int n);
-void delete_stack(struct stack_helper *sh);
-
-struct temp_mem *create_temp(int length);
-void *temp_malloc(size_t size, struct temp_mem *list);
-void free_temp(struct temp_mem *list);
-#endif
 
 #define stack_nonempty(sh) ((sh)->index > 0 || (sh)->next != NULL)
 
