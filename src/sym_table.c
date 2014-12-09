aa3403c79ca7559b7344a9af18b441cbf8b15325
Jacob Czech <czech.jacob@gmail.com>
2014-12-09 09:29:57 -0500

    Set uninitialised value.
    The volume for a region wasn't being initialised.

--- src/sym_table.c
+++ src/sym_table.c
@@ -341,6 +341,7 @@ struct region *new_region(void) {
   rp->area = 0.0;
   rp->flags = 0;
   rp->manifold_flag = MANIFOLD_UNCHECKED;
+  rp->volume = 0.0;
   rp->boundaries = NULL;
   rp->region_has_all_elements = 0;
   return rp;
