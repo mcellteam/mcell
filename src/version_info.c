48bdd4fdb18f4e22457788385f9be596cc9ac915
Jacob Czech <czech.jacob@gmail.com>
2015-02-10 14:07:24 -0500

    Removed old commented code.

--- src/version_info.c
+++ src/version_info.c
@@ -35,30 +35,11 @@
 const char mcell_version[] = MCELL_VERSION;
 
 /*
- * Prints out authors' institutions.  Ordering between the two institutions is
- * chosen randomly.
+ * Prints out authors' institutions.
  *
  *    f: file handle to which to write
  */
 void print_credits(FILE *f) {
-  // char const *institute[2];
-  //
-  // srand(time(NULL) / 2);
-  // if (rand() & 1)
-  //{
-  //  institute[0]="The Salk Institute for Biological Studies";
-  //  institute[1]="Pittsburgh Supercomputing Center, Carnegie Mellon
-  // University";
-  //}
-  // else
-  //{
-  //  institute[0]="Pittsburgh Supercomputing Center, Carnegie Mellon
-  // University";
-  //  institute[1]="The Salk Institute for Biological Studies";
-  //}
-  // fprintf(f,"  Copyright (C) 2006 - 2013 by\n    %s and\n    %s\n\n",
-  // institute[0], institute[1]);
-
   fprintf(
       f,
       "  Copyright (C) 2006 - 2014 by\n"
