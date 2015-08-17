82356706bc40a8ded1ab3f8796882aac8b834e89
Jacob Czech <czech.jacob@gmail.com>
2015-08-17 16:31:22 -0400

    Added function comments for DG parser.

--- src/dyngeom_parse_extras.c
+++ src/dyngeom_parse_extras.c
@@ -5,15 +5,26 @@
 #include "mcell_structs.h"
 #include "sym_table.h"
 
+/***************************************************************************
+ init_top_level_objs:
+  Create the region and object symbol tables for the DG parser. Create the top
+  level object and instance.
+
+ In:  dg_parse_vars: state of dynamic geometry parser
+ Out: Returns 1 on error, 0 on success.
+***************************************************************************/
 int init_top_level_objs(struct dyngeom_parse_vars *dg_parse_vars) {
+  // Create the region symbol table
   if ((dg_parse_vars->reg_sym_table = init_symtab(1024)) == NULL) {
     return 1;
   }
 
+  // Create the object symbol table
   if ((dg_parse_vars->obj_sym_table = init_symtab(1024)) == NULL) {
     return 1;
   }
 
+  // Create the top-level/world/root object
   struct sym_table *sym;
   if ((sym = store_sym(
       "WORLD_OBJ", OBJ, dg_parse_vars->obj_sym_table, NULL)) == NULL) {
@@ -26,6 +37,7 @@ int init_top_level_objs(struct dyngeom_parse_vars *dg_parse_vars) {
     return 1;
   }
 
+  // Create the top-level/world/root instance
   if ((sym = store_sym(
       "WORLD_INSTANCE", OBJ, dg_parse_vars->obj_sym_table, NULL)) == NULL) {
     return 1;
@@ -42,6 +54,13 @@ int init_top_level_objs(struct dyngeom_parse_vars *dg_parse_vars) {
   return 0;
 }
 
+/***************************************************************************
+ setup_root_obj_inst:
+  This needs to be called before reading every new DG file.
+
+ In:  dg_parse_vars: state of dynamic geometry parser
+ Out: Returns 1 on error, 0 on success.
+***************************************************************************/
 int setup_root_obj_inst(struct dyngeom_parse_vars *dg_parse_vars) {
   struct sym_table *sym;
 
@@ -58,6 +77,19 @@ int setup_root_obj_inst(struct dyngeom_parse_vars *dg_parse_vars) {
   return 0;
 }
 
+/***************************************************************************
+ dg_start_object:
+    Create a new object, adding it to the DG symbol table. The qualified name
+    of the object will be built by adding to the object_name_list, and the
+    object is made the "current_object" in the mdl parser state. Because of
+    these side effects, it is vital to call dg_finish_object at the end of the
+    scope of the object created here.
+
+ In:  dg_parse_vars: state of dynamic geometry parser
+      name: unqualified object name
+ Out: the newly created object
+ Note: This is similar to mdl_start_object.
+***************************************************************************/
 struct sym_table *dg_start_object(
     struct dyngeom_parse_vars *dg_parse_vars,
     char *name) {
@@ -94,6 +126,17 @@ struct sym_table *dg_start_object(
   return sym_ptr;
 }
 
+/***************************************************************************
+ dg_finish_object:
+    "Finishes" a new object, undoing the state changes that occurred when the
+    object was "started". This means popping the name off of the object name
+    stack, and resetting current_object to its value before this object was
+    defined.
+
+ In:  dg_parse_vars: state of dynamic geometry parser
+ Out: none
+ Note: This is similar to mdl_finish_object.
+***************************************************************************/
 void dg_finish_object(struct dyngeom_parse_vars *dg_parse_vars) {
   struct object_creation obj_creation;
   obj_creation.object_name_list_end = dg_parse_vars->object_name_list_end;
@@ -103,6 +146,16 @@ void dg_finish_object(struct dyngeom_parse_vars *dg_parse_vars) {
   dg_parse_vars->current_object = dg_parse_vars->current_object->parent;
 }
 
+/***************************************************************************
+ dg_create_region:
+    Create a named region on an object.
+
+ In:  reg_sym_table: the symbol table for all the regions
+      objp: fully qualified object name
+      name: name of the region to define
+ Out: The new region.
+ Note: This is similar to mdl_create_region.
+***************************************************************************/
 struct region *dg_create_region(
     struct sym_table_head *reg_sym_table,
     struct object *objp,
@@ -126,6 +179,20 @@ struct region *dg_create_region(
   return rp;
 }
 
+/***************************************************************************
+ dg_make_new_region:
+    Create a new region, adding it to the DG symbol table.
+
+    full region names of REG type symbols stored in main symbol table have the
+    form:
+         metaobj.metaobj.poly,region_last_name
+
+ In:  reg_sym_table: the symbol table for all the regions
+      objp: fully qualified object name
+      region_last_name: name of the region to define
+ Out: The newly created region
+ Note: This is similar to mdl_make_new_region.
+***************************************************************************/
 struct region *dg_make_new_region(
     struct sym_table_head *reg_sym_table,
     char *obj_name,
