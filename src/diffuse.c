ce163062d1dfc1878666fb8d8fa34c1e134cdfc1
Jacob Czech <czech.jacob@gmail.com>
2015-12-03 10:22:13 -0500

    Initial attempt to get SMs to work with PBCs.
    This is a crude initial attempt to get surface molecules to work with
    periodic boundary conditions and is currently not functional. It is
    based off of the code used for counting enclosed surface molecules.

--- src/diffuse.c
+++ src/diffuse.c
@@ -292,14 +292,15 @@ void pick_displacement(struct vector3 *v, double scale, struct rng_state *rng) {
 
 /*************************************************************************
 ray_trace_2d:
-  In: molecule that is moving
-      displacement vector from current to new location
-      place to store new coordinate (in coord system of new wall)
-      flag that tells that molecule hits ABSORPTIVE region border
+  In: world:
+      sm: molecule that is moving
+      disp: displacement vector from current to new location
+      pos: place to store new coordinate (in coord system of new wall)
+      kill_me: flag that tells that molecule hits ABSORPTIVE region border
            (value = 1)
-      reaction object (valid only in case of hitting ABSORPTIVE region
+      rxp: reaction object (valid only in case of hitting ABSORPTIVE region
          border)
-      region border hit data information
+      hd_info: region border hit data information
   Out: wall at endpoint of movement vector, plus location of that endpoint
        in the coordinate system of the new wall.
 *************************************************************************/
@@ -325,8 +326,53 @@ struct wall *ray_trace_2d(struct volume *world, struct surface_molecule *sm,
     int absorb_now = 0;
     int reflect_now = 0;
     struct vector2 boundary_pos;
-    index_edge_was_hit = find_edge_point(this_wall, &this_pos, &this_disp,
-      &boundary_pos);
+    index_edge_was_hit = find_edge_point(
+        this_wall, &this_pos, &this_disp, &boundary_pos);
+
+    // The following code is an initial attempt to get surface molecules to
+    // work with periodic boundary conditions. It's based off of the code for
+    // counting enclosed surface molecules (see count_moved_surface_mol).
+    struct vector3 origin;
+    struct vector3 target;
+    uv2xyz(&this_pos, this_wall, &origin);
+    if (index_edge_was_hit == -1) {
+      pos->u = this_pos.u + this_disp.u;
+      pos->v = this_pos.v + this_disp.v;
+      uv2xyz(pos, this_wall, &target);
+    }
+    else {
+      uv2xyz(&boundary_pos, this_wall, &target);
+    }
+    struct vector3 delta = {target.x - origin.x,
+                            target.y - origin.y,
+                            target.z - origin.z};
+    struct vector3 here = origin;
+    for (struct subvolume *sv = find_subvolume(world, &origin, NULL);
+         sv != NULL; sv = next_subvol(
+            &here, &delta, sv, world->x_fineparts, world->y_fineparts,
+            world->z_fineparts, world->nx_parts, world->ny_parts,
+            world->nz_parts)) {
+
+      int j = 0;
+      for (struct wall_list *wl = sv->wall_head; wl != NULL; wl = wl->next) {
+        if (wl->this_wall == sm->grid->surface) {
+          continue; // ignore the origin wall
+        }
+
+        struct vector3 hit = {0.0, 0.0, 0.0};
+        double t = 0.0;
+        j = collide_wall(&here, &delta, wl->this_wall, &t, &hit, 0, world->rng,
+          world->notify, &(world->ray_polygon_tests));
+        if (j != COLLIDE_MISS && (hit.x - target.x) * delta.x +
+            (hit.y - target.y) * delta.y + (hit.z - target.z) * delta.z < 0) {
+          // Trying to reflect off of the point (i.e. "hit") where we collided
+          // with the periodic box. This definitely isn't right, but I'm
+          // leaving it here for now for now.
+          xyz2uv(&hit, this_wall, &boundary_pos);
+          goto check_for_reflection; 
+        }
+      }
+    }
 
     /* Ambiguous edge collision--just give up */
     if (index_edge_was_hit == -2) {
