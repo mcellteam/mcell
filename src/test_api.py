bf34454e1370321a1f6c14cdae978a4551984386
Jacob Czech <czech.jacob@gmail.com>
2016-08-10 18:39:33 +0000

    Update test_api.py to create viz data.
    It seems like viz data is being generated correctly, but it's empty
    because there is probably still something wrong with releases. The
    releases are never reported at the command line and MCell never enters
    release_molecules.

--- src/test_api.py
+++ src/test_api.py
@@ -4,8 +4,9 @@ world = m.mcell_create()
 
 m.mcell_init_state(world)
 
+iterations = 100
 m.mcell_set_time_step(world,1e-6)
-m.mcell_set_iterations(world,100)
+m.mcell_set_iterations(world,iterations)
 
 species_def = m.mcell_species_spec()
 species_def.name = "x"
@@ -44,6 +45,9 @@ print(x_foo)
 rel_object = m.object()
 release_object = m.mcell_create_geometrical_release_site( world , scene, "B_releaser", m.SHAPE_SPHERICAL, position, diameter, x_foo, 5000.0, 1.0, None, rel_object)
 
+viz_list = m.mcell_add_to_species_list(mcell_sym, False, 0, None);
+m.mcell_create_viz_output(world, "./viz_data/test", viz_list, 0, iterations, 1)
+
 print (x_foo)
 print ("deleting x_foo")
 m.mcell_delete_species_list(x_foo)
@@ -62,4 +66,5 @@ print (release_object)
 
 
 m.mcell_init_simulation(world)
+m.mcell_init_output(world)
 m.mcell_run_simulation(world)
