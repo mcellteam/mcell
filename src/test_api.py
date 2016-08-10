42ce6088bbb1c0497c394712872ff26bb705d335
Evan Cresswell <evancresswell@gmail.com>
2016-08-10 13:58:10 -0400

    adding my test_api.py to src directory

--- src/test_api.py
+++ src/test_api.py
@@ -23,12 +23,37 @@ scene = m.object()
 m.mcell_create_instance_object(world, "Scene", scene)
 
 position = m.vector3()
-# { 0.0, 0.0, 0.0 };
-#struct vector3 diameter = { 0.00999, 0.00999, 0.00999 };
+position.x = 0.0
+position.y = 0.0
+position.z = 0.0
+print("position")
+print position
+diameter = m.vector3()
+diameter.x = .00999
+diameter.y = .00999
+diameter.z = .00999
 
-#struct object *B_releaser = NULL;
+x_releaser = m.object()
+print ("x_releaser")
+print x_releaser
 
-#struct mcell_species *B = mcell_add_to_species_list(molB_ptr, false, 0, NULL);
+x_foo= m.mcell_add_to_species_list(  mcell_sym  , False, 0, None);
+print("x_foo")
+print(x_foo)
+
+rel_object = m.object()
+release_object = m.mcell_create_geometrical_release_site( world , scene, "B_releaser", m.SHAPE_SPHERICAL, position, diameter, x_foo, 5000.0, 1.0, None, rel_object)
+
+print (x_foo)
+print ("deleting x_foo")
+m.mcell_delete_species_list(x_foo)
+print("printing whats left of x_foo")
+print(x_foo)
+
+
+print("x_releaser")
+print (release_object)
+
 
 #mcell_create_geometrical_release_site(state, world_object, "B_releaser", SHAPE_SPHERICAL, &position, &diameter, B, 5000, 1, NULL, &B_releaser), "could not create B_releaser");
 
