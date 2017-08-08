e03378aa0f94446bbde3047993c508da5ccb5fe5
Jacob Czech <czech.jacob@gmail.com>
2017-08-08 18:04:17 -0400

    Update unit tests.

--- src/pymcell_unittests.py
+++ src/pymcell_unittests.py
@@ -4,11 +4,11 @@ import pymcell as m
 class SimpleSpeciesTestCase(unittest.TestCase):
     def setUp(self):
         self.vm1 = m.Species("vm1", 1e-6)
-        self.vm1_down = m.OrientedSpecies(vm1, m.Orient.down)
+        self.vm1_down = m.OrientedSpecies(self.vm1, m.Orient.down)
         self.sm1 = m.Species("sm1", 1e-6, surface=True)
-        self.sm1_up = m.OrientedSpecies(sm1, m.Orient.up)
+        self.sm1_up = m.OrientedSpecies(self.sm1, m.Orient.up)
         self.vm2 = m.Species("vm2", 1e-6)
-        self.vm2_down = m.OrientedSpecies(vm2, m.Orient.down)
+        self.vm2_down = m.OrientedSpecies(self.vm2, m.Orient.down)
 
     def test_mol_is_3d(self):
         assert self.vm1.surface == False, "Volume molecule has 'surface' True"
@@ -19,7 +19,7 @@ class SimpleSpeciesTestCase(unittest.TestCase):
 
 class SimpleReactionTestCase(SimpleSpeciesTestCase):
     def test_rxn(self):
-        rxn = m.Reaction((vm1_down, sm1_up), (vm2_down, ), 1e8, name="create_vm2")
+        rxn = m.Reaction((self.vm1.down(), self.sm1.up()), (self.vm2.down(), ), 1e8, name="create_vm2")
 
 
 if __name__ == "__main__":
