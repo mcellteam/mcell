85d9f8e8dcb408f1f9ba7005965fecca2b842a0b
Jacob Czech <czech.jacob@gmail.com>
2018-06-04 18:47:32 -0400

    CMake working under Windows.
    This was already accomplished in the cmake_windows branch, but this is
    the first time getting it to work with the nfsim library needed for
    MCellR. More work is still neeeded to get pyMCell working with CMake
    under Windows.

--- requirements.py
+++ requirements.py
@@ -12,8 +12,8 @@ def nfsim():
     except OSError:
         pass
     os.chdir('lib')
-    subprocess.call(['cmake', '..'])
-    subprocess.call(['make'])
+    subprocess.call(['cmake', '-G', 'Ninja', '..'])
+    subprocess.call(['ninja'])
     os.chdir('..')
     os.chdir('..')
 
@@ -30,18 +30,20 @@ def nfsim_lib():
     extension = "so"
     if (sys.platform == 'darwin'):
         extension = "dylib"
-    subprocess.call(['ln', '-s', os.path.join('..', '..', 'nfsim', 'lib', 'libNFsim.{0}'.format(extension))])
+    elif (sys.platform == 'win32'):
+        extension = "dll"
+    subprocess.call(['cmd', '/c', 'mklink', '.\libNFsim.{0}'.format(extension), os.path.join('..', '..', 'nfsim', 'lib', 'libNFsim.{0}'.format(extension))])
     os.chdir('..')
 
-    subprocess.call(['ln', '-s', os.path.join('..', 'nfsim', 'include')])
+    subprocess.call(['cmd', '/c', 'mklink', '/D', '.\include', os.path.join('..', 'nfsim', 'include')])
 
     try:
         os.mkdir('build')
     except OSError:
         pass
     os.chdir('build')
-    subprocess.call(['cmake', '..'])
-    subprocess.call(['make'])
+    subprocess.call(['cmake', '-G', 'Ninja', '..'])
+    subprocess.call(['ninja'])
     os.chdir('..')
     os.chdir('..')
 
@@ -51,12 +53,24 @@ def copy_library_files():
         os.mkdir('build/lib')
     except OSError:
         pass
-    os.chdir('build/lib')
-    extension = "so"
-    if (sys.platform == 'darwin'):
+    os.chdir('build')
+
+    if (sys.platform == 'linux'):
+        extension = "so"
+    elif (sys.platform == 'darwin'):
         extension = "dylib"
-    shutil.copy(os.path.join('..', '..', 'nfsim', 'lib', 'libNFsim.{0}'.format(extension)), ".")
-    shutil.copy(os.path.join('..', '..', 'nfsimCInterface', 'build', 'libnfsim_c.{0}'.format(extension)), ".")
+    elif (sys.platform == 'win32'):
+        extension = "dll"
+
+    # XXX: For some reason, Windows will build MCell with the files in
+    # ./build/lib but will fail at runtime if they aren't in the same directory
+    # as the executable itself (i.e. the libs must be in ./build at runtime)
+    if (sys.platform == 'win32'):
+        shutil.copy(os.path.join('..', 'nfsim', 'lib', 'libNFsim.{0}'.format(extension)), ".")
+        shutil.copy(os.path.join('..', 'nfsimCInterface', 'build', 'libnfsim_c.{0}'.format(extension)), ".")
+
+    shutil.copy(os.path.join('..', 'nfsim', 'lib', 'libNFsim.{0}'.format(extension)), "lib")
+    shutil.copy(os.path.join('..', 'nfsimCInterface', 'build', 'libnfsim_c.{0}'.format(extension)), "lib")
 
 
 if __name__ == "__main__":
