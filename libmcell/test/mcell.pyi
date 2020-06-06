e11cc1f51ef0eea78f7f59f829104a59786c45ec
Adam Husar <ahusar@salk.edu>
2020-06-06 11:25:28 -0700

    Removed obsolete/not needed files from libmcell/test

--- libmcell/test/mcell.pyi
+++
@@ -1,7 +0,0 @@
-
-# we will need to generate a .pyi file so that the code completion in pyDev and PyCharm works
-class ReleaseSite():
-    def __init__(self, name):
-        self.name = name
-        pass
-        
