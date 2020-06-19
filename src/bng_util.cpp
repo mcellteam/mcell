d5274230b823c0a188b9aa0793cbc6bef39d139d
Adam Husar <ahusar@salk.edu>
2020-06-19 08:08:43 -0700

    Using BNGL string in MCell3 ASCII viz output as species

--- src/bng_util.cpp
+++ src/bng_util.cpp
@@ -22,8 +22,166 @@
 
 #include "bng_util.h"
 
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+struct Node {
+  bool is_mol; // not component
+  string name;
+  string state; // for component
+  string compartment; // for molecule
+  vector<size_t> connections;
+};
+
+
+static Node convert_node_str(const string& s) {
+  assert(s.size() > 0);
+  Node res;
+
+  if (s[0] == 'c') {
+    res.is_mol = false;
+  }
+  else if (s[0] == 'm') {
+    res.is_mol = true;
+  }
+  else {
+    assert(false);
+  }
+
+  if (!res.is_mol) {
+    // component
+
+    size_t tilde = s.find('~');
+    assert(tilde != string::npos);
+    res.name = s.substr(2, tilde - 2);
+
+    size_t excl = s.find('!');
+    assert(excl != string::npos); // component must be connected
+    res.state = s.substr(tilde + 1, excl - (tilde + 1));
+    if (res.state == "NO_STATE") {
+      res.state = "";
+    }
+  }
+  else {
+    // molecule
+    size_t at = s.find('@');
+    res.name = s.substr(2, at - 2);
+
+    size_t excl = s.find('!');
+    if (excl == string::npos) {
+      excl = s.size();
+    }
+
+    res.compartment = s.substr(at + 1, excl - (at + 1));
+    if (res.compartment == "NOCOMPARTMENT") {
+      res.compartment = "";
+    }
+  }
+
+  // connections
+  size_t excl = s.find('!');
+  if (excl != string::npos) {
+    size_t pos = excl + 1;
+    do {
+      excl = s.find('!', pos);
+      if (excl == string::npos) {
+        excl = s.size();
+      }
+
+      string num = s.substr(pos, excl - pos);
+      res.connections.push_back(stoi(num));
+
+      pos = excl + 1;
+    } while (excl != s.size());
+  }
+
+  return res;
+}
+
+
+static void graph_pattern_to_nodes(const string pat, vector<Node>& nodes) {
+
+  size_t pos = 0;
+  size_t comma_or_end;
+  do {
+
+    comma_or_end = pat.find(',', pos);
+    if (comma_or_end == string::npos) {
+      comma_or_end = pat.size();
+    }
+
+    if (comma_or_end == string::npos) {
+      comma_or_end = pat.size();
+    }
+
+    string node_str = pat.substr(pos, comma_or_end - pos);
+    Node n = convert_node_str(node_str);
+    nodes.push_back(n);
+
+    pos = comma_or_end;
+    pos++;
+  } while (comma_or_end != pat.size() && pos != pat.size());
+}
+
+
+static std::string nodes_to_bngl(const vector<Node>& nodes) {
+  string res;
+
+  for (size_t mol_index = 0; mol_index < nodes.size(); mol_index++) {
+    const Node& mol = nodes[mol_index];
+    if (mol.is_mol) {
+      res += mol.name;
+      if (!mol.connections.empty()) {
+        res += "(";
+
+        for (size_t component_index: mol.connections) {
+          const Node& comp = nodes[component_index];
+
+          res += comp.name;
+          if (comp.state != "") {
+            res += "~" + comp.state;
+          }
+
+          for (size_t conn: comp.connections) {
+            if (conn == mol_index) {
+              // connection to myself
+              continue;
+            }
+            res += "!" + to_string(conn);
+          }
+
+        }
+        res += ")";
+      }
+
+      if (mol.compartment != "") {
+        res += "@" + mol.compartment;
+      }
+
+      // expecting that all the molecules are at the end
+      if (mol_index + 1 != nodes.size()) {
+        res += ".";
+      }
+    }
+  }
+
+  return res;
+}
+
+
 std::string graph_pattern_to_bngl(const char* graph_pattern) {
-  // TODO
-  return graph_pattern;
+
+  // each c: or m: is a node on graph, ! specifies to what it is connected
+  // c:a~R!2!1,c:b~T!3!0,m:A@NOCOMPARTMENT!0,m:B@NOCOMPARTMENT!1
+
+  vector<Node> nodes;
+
+  // process the nauty string
+  graph_pattern_to_nodes(graph_pattern, nodes);
+
+  // make BNGL representation
+  return nodes_to_bngl(nodes);
 }
 
