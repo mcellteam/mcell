6de9c58cf8b9e1a6675083ecfaf9a37032938c18
Adam Husar <ahusar@salk.edu>
2020-08-09 13:21:48 -0700

    Extended SortMolsBySubpartEvent to sort also partition molecules, not just their ids

--- src4/sort_mols_by_subpart_event.cpp
+++ src4/sort_mols_by_subpart_event.cpp
@@ -28,6 +28,12 @@
 #include "world.h"
 #include "partition.h"
 
+// only one can be defined
+#define SORT_BY_SUBPART_INDEX
+//#define SORT_BY_MEM_CUBE
+
+const int SUBPARTS_PER_MEM_CUBE = 4;
+
 using namespace std;
 
 namespace MCell {
@@ -39,54 +45,116 @@ void SortMolsBySubpartEvent::dump(const string ind) const {
 }
 
 
-struct SubpartComparator
+uint subpart_index_to_mem_cube_index(const Partition& p, const subpart_index_t subpart_index) {
+  IVec3 indices;
+  p.get_subpart_3d_indices_from_index(subpart_index, indices);
+
+  // "decimate" the subpart indices
+  indices = IVec3(indices / IVec3(SUBPARTS_PER_MEM_CUBE));
+
+  assert(indices.x < 256 && indices.y < 256 && indices.z < 256);
+  // we just need to encode the memory subpart number in some way
+  return indices.x + (indices.y << 8) + (indices.z << 16);
+}
+
+
+inline bool compare_mols(
+    const Partition& p,
+    const Molecule& m1,
+    const Molecule& m2
+) {
+  const BNG::Species& s1 = p.get_all_species().get(m1.species_id);
+  const BNG::Species& s2 = p.get_all_species().get(m2.species_id);
+
+  if (m1.is_vol() && m2.is_vol() && s1.can_diffuse() && s2.can_diffuse()) {
+#ifndef SORT_BY_MEM_CUBE
+    if (m1.v.subpart_index != m2.v.subpart_index) {
+      return m1.v.subpart_index < m2.v.subpart_index;
+    }
+    else {
+      return m1.id < m2.id;
+    }
+#else
+    uint mem_index1 = subpart_index_to_mem_cube_index(p, m1.v.subpart_index);
+    uint mem_index2 = subpart_index_to_mem_cube_index(p, m2.v.subpart_index);
+    if (mem_index1 != mem_index2) {
+      return mem_index1 < mem_index2;
+    }
+# ifdef SORT_BY_SUBPART_INDEX
+    else if (m1.v.subpart_index != m2.v.subpart_index) {
+      return m1.v.subpart_index < m2.v.subpart_index;
+    }
+# endif
+    else {
+      return m1.id < m2.id;
+    }
+#endif
+  }
+  else {
+    return m1.id < m2.id;
+  }
+}
+
+
+struct SubpartComparatorForId
 {
-  SubpartComparator(const Partition& p_)
+  SubpartComparatorForId(const Partition& p_)
     : p(p_) {
   }
 
-  // Compare 2 Player objects using name
   bool operator () (const molecule_id_t id1, const molecule_id_t id2) const
   {
     const Molecule& m1 = p.get_m(id1);
     const Molecule& m2 = p.get_m(id2);
-    const BNG::Species& s1 = p.get_all_species().get(m1.species_id);
-    const BNG::Species& s2 = p.get_all_species().get(m2.species_id);
 
-    if (m1.is_vol() && m2.is_vol() && s1.can_diffuse() && s2.can_diffuse()) {
-      if (m1.v.subpart_index != m2.v.subpart_index) {
-        return m1.v.subpart_index < m2.v.subpart_index;
-      }
-      else {
-        return id1 < id2;
-      }
-    }
-    else {
-      return id1 < id2;
-    }
+    return compare_mols(p, m1, m2);
   }
 
   const Partition& p;
 };
 
 
+struct SubpartComparatorForMol
+{
+  SubpartComparatorForMol(const Partition& p_)
+    : p(p_) {
+  }
+
+  bool operator () (const Molecule& m1, const Molecule& m2) const
+  {
+    return compare_mols(p, m1, m2);
+  }
+
+  const Partition& p;
+};
+
 void SortMolsBySubpartEvent::step() {
   for (Partition& p: world->get_partitions()) {
-    vector<Molecule>& volume_molecules = p.get_molecules();
+    vector<Molecule>& molecules = p.get_molecules();
 
-    if (volume_molecules.empty()) {
+    if (molecules.empty()) {
       // simply skip if there are no volume molecules
       continue;
     }
 
-    // TODO: this can be optimized if we would take just
-    // volume mols into account
+    // NOTE: this can be optimized if we would take just volume mols into account
     vector<Partition::TimeStepMoleculesData>& mols_per_time_step = p.get_molecule_data_per_time_step_array();
 
     for (Partition::TimeStepMoleculesData& time_step_data: mols_per_time_step) {
-      std::vector<molecule_id_t>& molecule_ids = time_step_data.molecule_ids;
+      vector<molecule_id_t>& molecule_ids = time_step_data.molecule_ids;
 
-      std::sort(molecule_ids.begin(), molecule_ids.end(), SubpartComparator(p));
+      sort(molecule_ids.begin(), molecule_ids.end(), SubpartComparatorForId(p));
+    }
+
+    // also sort molecules in the molecules array
+    sort(molecules.begin(), molecules.end(), SubpartComparatorForMol(p));
+
+    // and update their indices in the molecule_id_to_index_mapping
+    std::vector<molecule_index_t>& molecule_id_to_index_mapping = p.get_molecule_id_to_index_mapping();
+    for (size_t i = 0; i < molecules.size(); i++) {
+      const Molecule& m = molecules[i];
+      assert(molecule_id_to_index_mapping.size() < m.id);
+      molecule_id_to_index_mapping[m.id] = i;
     }
   }
 }
