15def8dffa27466fc920c6540569163a7d76b553
Adam Husar <ahusar@salk.edu>
2020-11-18 07:22:11 -0800

    Updated plotter script to use pandas

--- utils/plotter/plotter.py
+++ utils/plotter/plotter.py
@@ -37,22 +37,21 @@ def get_mcell_observables_counts(dir):
                 observable = os.path.splitext(file)[0]
                 if observable.endswith('_MDLString'):
                     observable = observable[:-len('_MDLString')]
-                    
-                with open(file_path, 'r') as f:
-                    for line in f:
-                        if line:
-                            time_count = line.split()
-                            time = float(time_count[0])
-                            cnt = float(time_count[1])
                 
-                            if observable not in counts:
-                                counts[observable] = {}
-                                
-                            if time not in counts[observable]:
-                                counts[observable][time] = []
-                            
-                            counts[observable][time].append(cnt)
-            
+                if observable not in counts:
+                    index = 0
+                else:
+                    index = counts[observable].shape[1] - 1 
+                
+                col_name = 'count' + str(index)
+                df = pd.read_csv(file_path, sep=' ', names=['time', col_name])
+
+                if observable not in counts:
+                    counts[observable] = df
+                else:
+                    # add new column
+                    counts[observable][col_name] = df[col_name]
+                
     return counts 
 
 
@@ -60,27 +59,13 @@ def get_bng_observables_counts(file, counts):
     if not os.path.exists(file):
         print("Expected file " + file + " not found, skipping it")
         return
-     
+    
     with open(file, 'r') as f:
         first_line = f.readline()
-        observables = first_line.split()[1:]
-        for line in f:
-            if line:
-                line_counts = line.split() # no leading '#'
-            
-                assert len(observables) == len(line_counts)
-                time = float(line_counts[0])
-                for i in range(1, len(observables)):
-                    observable = observables[i]
-                    cnt = float(line_counts[i])
-                    
-                    if observable not in counts:
-                        counts[observable] = {}
-                        
-                    if time not in counts[observable]:
-                        counts[observable][time] = []
-                    
-                    counts[observable][time].append(cnt)
+        header = first_line.split()[1:]
+    df = pd.read_csv(file, delim_whitespace=True, comment='#', names=header)
+    return df
+    
                 
                 
 def get_nfsim_observables_counts(dir):
@@ -91,8 +76,21 @@ def get_nfsim_observables_counts(dir):
         full_dir = os.path.join(dir, nf_dir)
         if not nf_dir.startswith('nf_') or not os.path.isdir(full_dir):
             continue
-        get_bng_observables_counts(os.path.join(full_dir, 'test.gdat'), counts)
-            
+        df = get_bng_observables_counts(os.path.join(full_dir, 'test.gdat'), counts)
+        
+        # transform into separate dataframes based on observable 
+        for i in range(1, df.shape[1]):
+            observable = df.columns[i] 
+            if observable not in counts:
+                col_name = 'count0'  
+                # select time and the current observable
+                counts[observable] = pd.DataFrame()           
+                counts[observable]['time'] = df.iloc[:, 0]
+                counts[observable][col_name] = df.iloc[:, i]
+            else:
+                col_name = 'count' + str(counts[observable].shape[1] - 1)            
+                counts[observable][col_name] = df.iloc[:, i]  
+
     return counts 
 
 
@@ -140,25 +138,26 @@ for obs in sorted(counts[0]):
             print("Data for " + names[i] + " were not loaded.")
             continue
         
-        times = []
-        means = []
-        stds = []
+        data = counts[i][obs]
         
-        for time in sorted(counts[i][obs]):
-            times.append(time)
-            vals = np.array(counts[i][obs][time])
-            means.append(vals.mean())
-            stds.append(vals.std())
-        
-        ax.plot(times, means, label=obs + "1", c=clrs[i])
+        df = pd.DataFrame()           
+        df['time'] = data.iloc[:, 0]
+        df['means'] = data.iloc[:, 1:].mean(axis=1)
+        df['mean_minus_std'] = df['means'] - data.iloc[:, 1:].std(axis=1)
+        df['mean_plus_std'] = df['means'] + data.iloc[:, 1:].std(axis=1)
+
+        # free collected data to decrease memory consumption        
+        del data
+                
+        ax.plot(df['time'], df['means'], label=obs + "1", c=clrs[i])
         ax.fill_between(
-            times, 
-            np.array(means)-np.array(stds), np.array(means)+np.array(stds),
+            df['time'], 
+            df['mean_minus_std'], df['mean_plus_std'],
             alpha=0.3, facecolor=clrs[i])
 
         legend_names.append(names[i])
 
     plt.legend(legend_names)
     
-    plt.savefig(obs + '.png', dpi=1200)
+    plt.savefig(obs + '.png', dpi=600)
     plt.close(fig)
